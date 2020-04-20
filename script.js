e9a749c83334a8c9ba063acccfc441c5faa9d339
Kevin He <kevin.he@wustl.edu>
2020-04-20 11:23:14 -0500

    adjustments to framework

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const sections = [...document.querySelectorAll("section")]
 
 let options = {
   rootMargin: "0px",
-  threshold: 0.25
+  threshold: 0.35
 }
 
 const callback = (entries, observer) => {
   entries.forEach(entry => {
 		const { target } = entry
 		
-		if (entry.intersectionRatio >= 0.25) {
+		if (entry.intersectionRatio >= 0.35) {
 			target.classList.add("is-visible")
 		} else {
 			target.classList.remove("is-visible")
